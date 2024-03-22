dc93cb169d088646e7d189ac091e7b0b57f56347
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-22 12:46:29 +0000

    remove circular import

--- models/city.py
+++ models/city.py
@@ -9,3 +9,7 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey(State.id), nullable= False)
     name = Column(String(128), nullable= False)
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -5,6 +5,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
+from models.city import City
+
  
 
 class State(BaseModel, Base):
@@ -13,16 +15,16 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable= False)
     cities = relationship("City", cascade="all, delete-orphan", backref="state")
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            from models import storage
-            cities_list=[]
-            for city in storage.all(models.City).values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
-    
     def __init__(self, *args, **kwargs):
         """Initialize State object"""
         super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
