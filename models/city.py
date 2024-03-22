db60b72c0157a95b9248bd727fbfaaa6e460529f
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-22 12:48:27 +0000

    remove circular import

--- models/city.py
+++ models/city.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, ForeignKey, String
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey(State.id), nullable= False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable= False)
     name = Column(String(128), nullable= False)
 
     def __init__(self, *args, **kwargs):
