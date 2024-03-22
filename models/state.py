6605f4b5eb790e81e30d0f91ec7255b9bc3465ec
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-22 12:43:17 +0000

    remove circular import

--- models/state.py
+++ models/state.py
@@ -22,3 +22,7 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     cities_list.append(city)
             return cities_list
+    
+    def __init__(self, *args, **kwargs):
+        """Initialize State object"""
+        super().__init__(*args, **kwargs)
