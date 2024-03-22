3cdb77dda8f34e6ef68d1bc0e410234b7159e372
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-22 12:29:40 +0000

    remove circular import

--- models/base_model.py
+++ models/base_model.py
@@ -9,22 +9,31 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = Column(String(60), primary_key=True, nullable= False)
-            self.created_at = Column(DateTime, nullable= False, default = datetime.datetime.now)
-            self.updated_at = Column(DateTime, nullable= False, default = datetime.datetime.now)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
-            # del kwargs['__class__']
-            # self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        if not self.id:
+            self.id = str(uuid.uuid4())
+        d = datetime.now()
+        if not self.created_at:
+            self.created_at = self.updated_at = d
+        if not self.updated_at:
+            self.updated_at = d
 
     def __str__(self):
         """Returns a string representation of the instance"""
