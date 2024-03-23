3f046e644dd9a037b298d08810b9c724deaa5f8e
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-23 23:35:40 +0000

    Update review

--- models/amenity.py
+++ models/amenity.py
@@ -20,25 +20,3 @@ class Amenity(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes amenities"""
         super().__init__(*args, **kwargs)
-
-
-#!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.place import Place
-from models.user import User
-
-class Review(BaseModel, Base):
-    """ Review class to store review information """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey(Place.id), nullable=False)
-        user_id = Column(String(60), ForeignKey(User.id), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
