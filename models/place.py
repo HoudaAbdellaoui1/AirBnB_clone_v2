5b396af51c3adfcd7240b2cea3b679f46ffda9dd
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-23 16:29:26 +0000

    Place update

--- models/place.py
+++ models/place.py
@@ -6,10 +6,10 @@ from os import getenv
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if getenv('HBNB_STORAGE_TYPE') == 'db':
         __tablename__ = "places"
-        city_id = Column(String(60), nullable=False, ForeignKey="cities.id")
-        user_id = Column(String(60), nullable=False, ForeignKey="users.id")
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
