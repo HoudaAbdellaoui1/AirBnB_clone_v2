a2ff1112f2cb639166ed95cfc7e19a792941ccbc
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-07 22:56:03 +0000

    remove sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 # Prepare web server
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install nginx
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-mkdir -p /data/web_static/{releases/test,shared}
-
+apt-get update -y
+apt-get upgrade -y
+apt-get install nginx -y
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
 echo "test deploying web_static" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test /data/web_static/current
-
-chown -R root:root /data/web_static
-
-config_block="location /hbnb_static {
-        alias /data/web_static/current/;
-        index index.html;
-    }"
-
-sed -i "/server_name _;/a $config_block" /etc/nginx/sites-available/default
+ln -fs /data/web_static/releases/test /data/web_static/current
+chown -R root:root /data
+sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
 service nginx restart
