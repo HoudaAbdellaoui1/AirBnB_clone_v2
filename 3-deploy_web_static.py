4446550494f593d0bb9d2aaecdfdb810e872b335
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-07 23:31:11 +0000

    Deploy archive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,37 +4,39 @@ Script to create and distribute an archive to the
 web servers
 """
 
-from fabric.api import env, local, put, run
-from datetime import datetime
+from fabric.api import put, run, env, local
 from os.path import exists, isdir
+from datetime import datetime
 env.hosts = ['35.153.33.193', '54.174.11.67']
 
 
-def do_pack():
-    """function to generate the tgz archive"""
+def dopack():
+    """generates an archive to my web servers"""
+
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        file_n = "versions/web_static{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_n))
+        return file_n
     except:
         return None
 
-
 def do_deploy(archive_path):
-    """distributes an archive """
+    """distributes an archive to my web servers"""
+
+
     if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
@@ -45,7 +47,9 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """create & distribute archive"""
+    """creates and distributes an archive to the web servers"""
+
+
     archive_path = do_pack()
     if archive_path is None:
         return False
