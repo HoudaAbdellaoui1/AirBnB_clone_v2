35cf677fef4e92394cd4e728312384e1fcef57f6
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-04-23 16:31:33 +0000

    replace remove by close session

--- models/__init__.py
+++ models/__init__.py
@@ -5,9 +5,9 @@ from os import getenv
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
     
 elif getenv('HBNB_TYPE_STORAGE') == 'file':
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
